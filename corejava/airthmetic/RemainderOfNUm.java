9a68293abc2037eb1d7b41490028dcd4d18dbf20
Aameen-SE <aameen.se@gmail.com>
2024-08-02 15:41:53 +0530

    find the remainder of the num

---
+++ corejava/airthmetic/RemainderOfNUm.java
@@ -0,0 +1,9 @@
+package corejava.airthmetic;
+
+public class RemainderOfNUm {
+    public static void main(String[] args) {
+        int x=37,y=5;
+        int z=x%y;
+        System.out.println("Remainder of a given number :"+z);
+    }
+}
