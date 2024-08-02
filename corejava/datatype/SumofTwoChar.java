66cb11d8623cfb615de342cc79cfc8a57a0a96c9
Aameen-SE <aameen.se@gmail.com>
2024-08-02 15:40:30 +0530

    sum of the number

---
+++ corejava/datatype/SumofTwoChar.java
@@ -0,0 +1,11 @@
+package corejava.datatype;
+
+public class SumofTwoChar {
+    public static void main(String[] args) {
+        //wap to declare tow char variable and assign a value then add these two variables and print the sum
+        char a='A';
+        char b='B';
+        char sum =(char)(a+b);
+        System.out.println("sum of two char var : "+sum);
+    }
+}
