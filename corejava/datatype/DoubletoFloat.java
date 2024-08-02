8b3ce31277bb9e345f0acbb3bf1f6e9de9142df6
Aameen-SE <aameen.se@gmail.com>
2024-08-02 15:39:10 +0530

    type casting double to float

---
+++ corejava/datatype/DoubletoFloat.java
@@ -0,0 +1,12 @@
+package corejava.datatype;
+
+public class DoubletoFloat {
+    public static void main(String[] args) {
+        //wap to define a double and assing it to a float variable and print both variables
+        double d=13;
+        float f=(float)d;
+
+        System.out.println("double var d:"+d);
+        System.out.println("float var f:"+f);
+    }
+}
