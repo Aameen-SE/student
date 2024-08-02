5d5178056a94a814d8a044446e0677f61de3c77a
Aameen-SE <aameen.se@gmail.com>
2024-08-02 15:38:08 +0530

    type casting byte to short

---
+++ corejava/datatype/BytetoShort.java
@@ -0,0 +1,10 @@
+package corejava.datatype;
+
+public class BytetoShort {
+    public static void main(String[] args) {
+        byte a=1;
+        short b = a;
+
+        System.out.println("byte a:"+a+" short b:"+b);
+    }
+}
