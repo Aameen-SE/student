a693995ac5f6d36f6c631aed443429a12a07e2cf
Aameen-SE <aameen.se@gmail.com>
2024-08-02 15:38:42 +0530

    type casting char to int

---
+++ corejava/datatype/ChartoInt.java
@@ -0,0 +1,12 @@
+package corejava.datatype;
+
+public class ChartoInt {
+    public static void main(String[] args) {
+        //wap to create a char variable but assign an int value and print that variable?
+        char var =65;
+        char v=(int)65;
+        System.out.println("char var:"+var);
+        System.out.println("char var:"+var);
+
+    }
+}
