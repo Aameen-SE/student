79532715b35d0053b19c6cabd017e9d0b3a2ea09
Aameen-SE <aameen.se@gmail.com>
2024-08-06 02:15:18 +0530

    oops concept

---
+++ corejava/aug05/SquareMain.java
@@ -0,0 +1,8 @@
+package corejava.aug05;
+
+public class SquareMain {
+    public static void main(String[] args) {
+        Square square=new Square(5);
+        System.out.println(square.calculateArea());
+    }
+}
