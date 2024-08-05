8a823133a5d13baa711c1e38328471ab70780298
Aameen-SE <aameen.se@gmail.com>
2024-08-05 13:43:08 +0530

    Printing of array length

---
+++ corejava/aug04/PrintArrayLength.java
@@ -0,0 +1,12 @@
+package corejava.aug04;
+
+public class PrintArrayLength {
+    public static void main(String[] args) {
+        //wap to find length of an array
+        int [] arr=new int[15];
+        System.out.println("length of first array :"+arr.length);
+
+        int [] arr1 = {54 , 36, 78 , 95};
+        System.out.println("length of second array :"+arr1.length);
+    }
+}
