6219a80a03011a72f9bc6d03280c3c35b7278638
Aameen-SE <aameen.se@gmail.com>
2024-08-05 13:42:47 +0530

    Printing of array

---
+++ corejava/aug04/PrintArray.java
@@ -0,0 +1,18 @@
+package corejava.aug04;
+
+import java.util.Arrays;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        //wap to create and print array using without loop
+
+        int[] arr = new int[3];
+        arr[0]=23;
+        arr[1]=34;
+        arr[2]=54;
+
+        String [] arr1={"Aameen" , "Hussain" };
+       System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
+    }
+}
