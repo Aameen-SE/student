7a321d89444e3ea0147096c67e28333dafaa955c
Aameen-SE <aameen.se@gmail.com>
2024-08-06 02:13:09 +0530

    oops concept

---
+++ corejava/aug05/DimensionMain.java
@@ -0,0 +1,9 @@
+package corejava.aug05;
+
+public class DimensionMain {
+    public static void main(String[] args) {
+        Dimension dimension=new Dimension(25);
+        System.out.println(dimension.getFeeds());
+        System.out.println(dimension.getInches());
+    }
+}
