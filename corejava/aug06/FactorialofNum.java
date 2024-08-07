885c130db57fade464bfee7e2387c1fac779f4c7
Aameen-SE <aameen.se@gmail.com>
2024-08-07 20:03:25 +0530

    factorial of number

---
+++ corejava/aug06/FactorialofNum.java
@@ -0,0 +1,13 @@
+package corejava.aug06;
+
+public class FactorialofNum {
+    public static void main(String[] args) {
+        int num=5;
+        int fact=1;
+        for(int i=5;i<=1;i--){
+             fact=fact*i;
+        }
+        System.out.println("Factorial of a given number is :"+fact);
+    }
+
+}
