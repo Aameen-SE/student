8c577cadbd046a0e4ca71e8f78d46baff2abcf33
Aameen-SE <aameen.se@gmail.com>
2024-08-08 13:28:01 +0530

    oops concept

---
+++ corejava/aug07/City.java
@@ -0,0 +1,25 @@
+package corejava.aug07;
+
+public class City {
+    //Create a class City having member variables id, city. create getters and setters for all member variables. Create object of City class in
+    //		other class inside main method and set all member values and print all member values using getters.
+
+    private int id;
+    private String city;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
