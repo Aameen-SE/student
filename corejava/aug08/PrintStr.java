393cb5dd45796147a165bfc1a0535c98b586915a
Aameen-SE <aameen.se@gmail.com>
2024-08-09 17:10:03 +0530

    method question solve

---
+++ corejava/aug08/PrintStr.java
@@ -0,0 +1,14 @@
+package corejava.aug08;
+
+public class PrintStr {
+    public static void main(String[] args) {
+        String str ="This is text";
+
+
+
+        System.out.println(str.length());
+        System.out.println(str.substring(5));
+        System.out.println(str.substring(5,11));
+        System.out.println(str.charAt(2));
+    }
+}
