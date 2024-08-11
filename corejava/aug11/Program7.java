0f511833667c8d99e2c2442b31f9d609b0200db0
Aameen-SE <aameen.se@gmail.com>
2024-08-11 18:05:13 +0530

    todays program

---
+++ corejava/aug11/Program7.java
@@ -0,0 +1,12 @@
+package corejava.aug11;
+
+public class Program7 {
+    public static void main(String[] args) {
+        //Wap to find a given number is negative or not?
+        int num =-10;
+        if(num<0)
+            System.out.println("Negative num ");
+        else
+            System.out.println("Not negative ");
+    }
+}
