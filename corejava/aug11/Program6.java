17ca3821578d68e55f617b13ecf541e541a1c8a2
Aameen-SE <aameen.se@gmail.com>
2024-08-11 18:04:47 +0530

    todays program

---
+++ corejava/aug11/Program6.java
@@ -0,0 +1,14 @@
+package corejava.aug11;
+
+public class Program6 {
+    public static void main(String[] args) {
+        //		what will be the value of a and b on printing
+        int a = 1;
+        int b = 2;
+        if(a==1 || ++b==3){
+            System.out.println("hello world");
+        		}
+
+        System.out.println("a :"+a+" ; b :"+b);
+    }
+}
