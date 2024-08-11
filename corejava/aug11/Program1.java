08b5a835d0f1901be3995cc59113c05d65a20303
Aameen-SE <aameen.se@gmail.com>
2024-08-11 18:03:25 +0530

    todays program

---
+++ corejava/aug11/Program1.java
@@ -0,0 +1,10 @@
+package corejava.aug11;
+
+public class Program1 {
+    public static void main(String[] args) {
+        //Wap to add two int variable but assign the result to double variable and print the result?
+        int var1 = 12, var2=14;
+        double result = var1+var2;
+        System.out.println(result);
+    }
+}
