71033479dd6b3fb22baaefd696929d4edd0f4428
Aameen-SE <aameen.se@gmail.com>
2024-08-11 18:03:58 +0530

    todays program

---
+++ corejava/aug11/Program3.java
@@ -0,0 +1,10 @@
+package corejava.aug11;
+
+public class Program3 {
+    public static void main(String[] args) {
+        //Wap to add two char variable and store sum in char variable and print the result?
+        char var1 ='A', var2='B';
+        char sum= (char) (var1+var2);
+        System.out.println(sum);
+    }
+}
