8acb77510c23bbcec9848528eead594fafc7b6f9
Aameen-SE <aameen.se@gmail.com>
2024-08-11 18:04:14 +0530

    todays program

---
+++ corejava/aug11/Program4.java
@@ -0,0 +1,10 @@
+package corejava.aug11;
+
+public class Program4 {
+    public static void main(String[] args) {
+        //Wap to add two float variable but assign the result to int variable and print the result?
+        float var1=12f, var2=45f;
+        int result =(int) (var1+var2);
+        System.out.println(result);
+    }
+}
