ad3ec8ef191402afa2852788d1c84824868fca4f
Aameen-SE <aameen.se@gmail.com>
2024-08-11 13:22:21 +0530

    pattern

---
+++ corejava/aug09/CompareStrng.java
@@ -0,0 +1,11 @@
+package corejava.aug09;
+
+public class CompareStrng {
+    public static void main(String[] args) {
+        //How to compare strings?
+
+        String str = new String("50");
+        String str2= "50";
+        System.out.println(str.compareTo(str2));
+    }
+}
