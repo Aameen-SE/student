70d316ab5748b3f5ce9584e4c0aa50435202820d
Aameen-SE <aameen.se@gmail.com>
2024-08-12 16:32:26 +0530

    concatenation of string

---
+++ corejava/aug12/ConcateStrng.java
@@ -0,0 +1,11 @@
+package corejava.aug12;
+
+public class ConcateStrng {
+    public static void main(String[] args) {
+        String str = "This is ";
+        String str2="a string ";
+
+        System.out.println(str+str2);
+        System.out.println(str.concat(str2));
+    }
+}
