1f7b526e5f57c5e939ed7f0114e91922494225fe
Aameen-SE <aameen.se@gmail.com>
2024-08-12 16:32:48 +0530

    converting the string to uppercase

---
+++ corejava/aug12/ConvertUpperCase.java
@@ -0,0 +1,8 @@
+package corejava.aug12;
+
+public class ConvertUpperCase {
+    public static void main(String[] args) {
+        String str="this is a string ";
+        System.out.println(str.toUpperCase());
+    }
+}
