a2903e630257bc91adcc0969335dd65b95546862
Aameen-SE <aameen.se@gmail.com>
2024-08-12 16:33:52 +0530

    removing the space in  given string at starting and ending

---
+++ corejava/aug12/RemoveSpaceStartingEnding.java
@@ -0,0 +1,10 @@
+package corejava.aug12;
+
+public class RemoveSpaceStartingEnding {
+    public static void main(String[] args) {
+        //Wap to remove spaces those are present at starting and ending of a String
+            String str ="    Yahoo   ";
+
+        System.out.println(str.trim());
+    }
+}
