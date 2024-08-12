c181c8d996f9e0b7ae84c9c828f6c0b54fd75a75
Aameen-SE <aameen.se@gmail.com>
2024-08-12 16:34:59 +0530

    searching the word

---
+++ corejava/aug12/SearchWord.java
@@ -0,0 +1,11 @@
+package corejava.aug12;
+
+public class SearchWord {
+    public static void main(String[] args) {
+        //How to search a word inside a string?
+
+        String str = "Do you practice everyday ?";
+        System.out.println("Does String contains the given word available :"+str.contains("You"));
+
+    }
+}
