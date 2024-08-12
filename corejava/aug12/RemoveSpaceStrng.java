f42b0cc41661e5c664c60581cb3a0c6ce463b301
Aameen-SE <aameen.se@gmail.com>
2024-08-12 16:34:07 +0530

    removing the space in  given string

---
+++ corejava/aug12/RemoveSpaceStrng.java
@@ -0,0 +1,10 @@
+package corejava.aug12;
+
+public class RemoveSpaceStrng {
+    public static void main(String[] args) {
+        //Wap to remove all spaces from a string
+        String str = "this is a string";
+        System.out.println("original String :"+str);
+        System.out.println("After removing spaces : "+str.replace(" ",""));
+    }
+}
