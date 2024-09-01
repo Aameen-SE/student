ff0bae518d13ce786904768cd41b150329f235ab
Aameen-SE <aameen.se@gmail.com>
2024-09-01 09:55:28 +0530

    linkedlist using collection framework

---
+++ linklst/newLL.java
@@ -0,0 +1,12 @@
+package linklst;
+
+import java.util.LinkedList;
+
+public class newLL {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("is");
+        list.add("a");
+        System.out.println(list);
+    }
+}
