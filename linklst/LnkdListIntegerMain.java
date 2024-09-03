52caaed7bca3e373f53cbbc415f31afc7f9c098e
Aameen-SE <aameen.se@gmail.com>
2024-09-03 17:11:50 +0530

    linked list

---
+++ linklst/LnkdListIntegerMain.java
@@ -0,0 +1,12 @@
+package linklst;
+
+public class LnkdListIntegerMain {
+    public static void main(String[] args) {
+        LnkdListInteger list = new LnkdListInteger();
+        list.append(1);
+        list.printList();
+        list.append(2);
+        list.append(3);
+        list.printList();
+    }
+}
