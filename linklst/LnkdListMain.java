438b6154a5877961947871ab392d09ab4b84e9a1
Aameen-SE <aameen.se@gmail.com>
2024-09-03 17:12:06 +0530

    linked list

---
+++ linklst/LnkdListMain.java
@@ -0,0 +1,9 @@
+package linklst;
+
+public class LnkdListMain {
+    public static void main(String[] args) {
+        LnkdLstDemo list = new LnkdLstDemo();
+        list.append("this");
+        list.printList();
+    }
+}
