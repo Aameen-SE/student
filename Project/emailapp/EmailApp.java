6e4441b15ed60be73219d093fe3e17c9b78700e9
Aameen-SE <aameen.se@gmail.com>
2024-10-21 18:34:16 +0530

    Project Name : Email Administration Application in Java

---
+++ Project/emailapp/EmailApp.java
@@ -0,0 +1,8 @@
+package Project.emailapp;
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email em1 = new Email("Aameen","Hussain");
+        System.out.println(em1.showInfo());
+    }
+}
