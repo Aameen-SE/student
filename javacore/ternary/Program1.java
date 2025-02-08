bfe40009496639657a876c0200cc13e6f6144568
Aameen-SE <aameen.se@gmail.com>
2025-02-08 13:25:14 +0530

    Practice

---
+++ javacore/ternary/Program1.java
@@ -0,0 +1,12 @@
+package javacore.ternary;
+
+public class Program1 {
+    public static void main(String[] args) {
+        //wap to print greatest number among two given numbers( ternary operator).
+
+        int num1 = 45,num2=98;
+        int greatestNum = num1>num2 ?num1:num2;
+        System.out.println("Greatest num between two num is "+greatestNum);
+
+    }
+}
