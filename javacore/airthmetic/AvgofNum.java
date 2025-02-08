a380f0bbdeaf3779a40ae5cc7151270920b7107d
Aameen-SE <aameen.se@gmail.com>
2025-02-08 13:27:46 +0530

    Practice

---
+++ javacore/airthmetic/AvgofNum.java
@@ -0,0 +1,20 @@
+package javacore.airthmetic;
+
+public class AvgofNum {
+    public static void main(String[] args) {
+        int n1 =91,n2=87,n3=75;
+        // wap to find the avg of 2 number
+        float a = (n1+n2)/2;
+        System.out.println("Average of the two num is "+a);
+
+        //wap to find average of 3 numbers?
+
+        float avg = (n1+n2+n3)/3;
+        System.out.println("Average of the three num is :"+avg);
+
+        //wap to find average of 4 numbers?
+        int n4=64;
+        float av = (n1+n2+n3+n4)/4;
+        System.out.println("Average of the four num is "+av);
+    }
+}
