aba3f80c638cc49ac174c282ab6bd689dc28cfd5
Aameen-SE <aameen.se@gmail.com>
2025-02-08 13:24:50 +0530

    Practice

---
+++ javacore/ternary/Program3.java
@@ -0,0 +1,12 @@
+package javacore.ternary;
+
+public class Program3 {
+    public static void main(String[] args) {
+        //wap to print negative number among two given numbers( ternary operator).
+
+        int n1=9,n2=-9;
+        int negNum = n1<0?n1:n2;
+        System.out.println("Negative num among two num is "+negNum);
+
+    }
+}
