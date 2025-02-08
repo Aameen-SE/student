5ebb6770eb0ab8402b84a9231f2311370ea922be
Aameen-SE <aameen.se@gmail.com>
2025-02-08 13:28:59 +0530

    Practice

---
+++ javacore/airthmetic/Problem3.java
@@ -0,0 +1,19 @@
+package javacore.airthmetic;
+
+public class Problem3 {
+    public static void main(String[] args) {
+
+        //wap to multiply two numbers?
+
+        int num1 = 34,num2=98;
+        int mult = num1*num2;
+        System.out.println("Multiplication of two num : "+mult);
+
+        //	6.	wap to multiply three numbers?
+
+        int n1=3,n2=78,n3=65;
+        int m=n1*n2*n3;
+        System.out.println("Multiplication of three num is :"+m);
+
+    }
+}
