2965d73af841de0f243acd66f67f52f906584915
Aameen-SE <aameen.se@gmail.com>
2025-02-08 13:24:56 +0530

    Practice

---
+++ javacore/ternary/Program2.java
@@ -0,0 +1,10 @@
+package javacore.ternary;
+
+public class Program2 {
+    public static void main(String[] args) {
+        //wap to print smallest number among two given numbers(  ternary operator ).
+        int num1 = 87,num2=65;
+        int smallestNum = num1<num2?num1:num2;
+        System.out.println("Smallest number among two num is :"+smallestNum);
+    }
+}
