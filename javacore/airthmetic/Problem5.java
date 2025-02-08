d22c06d1713bc392f9b1629ca25abd6b9b9b0d61
Aameen-SE <aameen.se@gmail.com>
2025-02-08 13:27:28 +0530

    Practice

---
+++ javacore/airthmetic/Problem5.java
@@ -0,0 +1,10 @@
+package javacore.airthmetic;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        //wap to find percentage?
+        double totalnum=600, num=275 ;
+        double percentage = (num/totalnum)*100;
+        System.out.println("Percentage of the given number is :"+percentage);
+    }
+}
