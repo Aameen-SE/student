25a7b423ceb9af022d210f8ab00434e907324b7d
Aameen-SE <aameen.se@gmail.com>
2025-02-08 13:27:16 +0530

    Practice

---
+++ javacore/airthmetic/Problem6.java
@@ -0,0 +1,32 @@
+package javacore.airthmetic;
+
+public class Problem6 {
+    public static void main(String[] args) {
+
+        //Wap to add two int variable but assign the result to double variable and print the result?
+                    int n1 =76,n2=89;
+                    double result = n1+n2;
+                    System.out.println("Add of two int var in the double var is :"+result);
+
+        //Wap to add two int variable but assign the result to float variable and print the result?
+                    float var = n1+n2;
+                    System.out.println("Add of two int var in the float var is :"+var);
+
+        //	30. 	Wap to add two char variable and store sum in char variable and print the result?
+                    char num1 =76,num2=89;
+                    char sum = (char)(num1+num2);
+                    System.out.println("Result of sum of value in char variable :"+sum);
+
+        //	31. 	Wap to add two float variable but assign the result to int variable and print the result?
+                    float a1 = 32.4f,a2=56.3f;
+                    int re = (int) (a1+a2);
+                    System.out.println("Result in int var of sum of float var :"+re);
+
+        //	32. 	Wap to add two double variable but assign the result to int variable and print the result?
+                    double v1 = 34.4,v2=89.4;
+                    int r = (int)(v1+v2);
+                    System.out.println("Result of sum of double var in int var :"+r);
+
+
+    }
+}
