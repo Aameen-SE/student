56d5636ec976a6170493239cf50bc5ab141a92eb
Aameen-SE <aameen.se@gmail.com>
2025-02-08 13:28:41 +0530

    Practice

---
+++ javacore/airthmetic/Problem4.java
@@ -0,0 +1,10 @@
+package javacore.airthmetic;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        //wap to find remainder of a given numbers?
+        int num1 =99,num2=6;
+        int rem = num1%num2;
+        System.out.println("Remainder of two num is : "+rem);
+    }
+}
