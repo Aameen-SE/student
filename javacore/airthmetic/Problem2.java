7eb9e3469cdebf21207c1987573fa8c4a8109086
Aameen-SE <aameen.se@gmail.com>
2025-02-09 15:58:11 +0530

    Practice

---
+++ javacore/airthmetic/Problem2.java
@@ -0,0 +1,14 @@
+package javacore.airthmetic;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        //wap to subtract two numbers?
+        int num1 =97,num2=76;
+        int sub = num1-num2;
+        System.out.println("Subtract of two num is :"+sub);
+        //	4.	wap to subtract three numbers?
+        int n1=34 , n2=45,n3=89;
+        int s = n3-n2-n1;
+        System.out.println("Subtraction of three num is :"+s);
+    }
+}
