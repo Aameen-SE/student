872dd929eaf4b22b790af31b57cc39728f69a7d5
Aameen-SE <aameen.se@gmail.com>
2025-02-09 15:56:42 +0530

    Practice

---
+++ javacore/airthmetic/Problem1.java
@@ -0,0 +1,15 @@
+package javacore.airthmetic;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        //wap to add two numbers?
+        int num1 =74, num2=98;
+        int sum = num1+num2;
+        System.out.println("Sum of the two num : "+sum);
+
+        // wap to add three numbers?
+        int n1 =74, n2=98, n3 = 87;
+        int add = n1+n2+n3;
+        System.out.println("Addition  of the three num : "+add);
+    }
+}
