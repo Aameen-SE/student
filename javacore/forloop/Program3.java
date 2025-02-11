2fda75859833c20d0ea899796f8b757ba700ccc7
Aameen-SE <aameen.se@gmail.com>
2025-02-11 21:07:02 +0530

    Practice

---
+++ javacore/forloop/Program3.java
@@ -0,0 +1,12 @@
+package javacore.forloop;
+
+public class Program3 {
+    public static void main(String[] args) {
+        //wap to print numbers which is multiple of 3 between 1 to 22
+        for(int i =1;i<=22;i++){
+            if(i%3==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
