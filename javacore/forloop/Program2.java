eaaef6ff56d8331d511f2b893b34835776f7afe1
Aameen-SE <aameen.se@gmail.com>
2025-02-11 20:54:58 +0530

    Practice

---
+++ javacore/forloop/Program2.java
@@ -0,0 +1,13 @@
+package javacore.forloop;
+
+public class Program2 {
+    public static void main(String[] args) {
+        //wap to print number from 10 to 1
+        System.out.println("Printing the number from 10 to 1 ");
+        int num = 10;
+        for(int i =num;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
