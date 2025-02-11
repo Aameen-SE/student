c17772eb66afb83c8daf0f070ae4f8681d8bb45f
Aameen-SE <aameen.se@gmail.com>
2025-02-11 21:06:43 +0530

    Practice

---
+++ javacore/forloop/Program4.java
@@ -0,0 +1,10 @@
+package javacore.forloop;
+
+public class Program4 {
+    public static void main(String[] args) {
+        //wap to print 5 times A
+        for(int i =1;i<=5;i++){
+            System.out.println("A");
+        }
+    }
+}
