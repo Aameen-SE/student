4926e09f8fd274185734b03f85659942a5760ed4
Aameen-SE <aameen.se@gmail.com>
2025-02-11 20:54:10 +0530

    Practice

---
+++ javacore/forloop/Program1.java
@@ -0,0 +1,13 @@
+package javacore.forloop;
+
+public class Program1 {
+    public static void main(String[] args) {
+        //wap to print number from 1 to 10
+        System.out.println("wap to print number from 1 to 10 : ");
+        int num = 1;
+        for(int i = num;i<=10;i++){
+            System.out.println(i);
+        }
+
+    }
+}
