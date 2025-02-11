1c8046895e7c480d8aef4ab09094a684805389bd
Aameen-SE <aameen.se@gmail.com>
2025-02-11 21:06:31 +0530

    Practice

---
+++ javacore/forloop/Program5.java
@@ -0,0 +1,13 @@
+package javacore.forloop;
+
+public class Program5 {
+    public static void main(String[] args) {
+        //wap to print all even numbers between 100 to 120.
+        System.out.println("Print all even numbers between 100 to 120.");
+        for(int i =100;i<=120;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
