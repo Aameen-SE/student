6abed92eff2b3ce0e9b220b5462cc789dd423b69
Aameen-SE <aameen.se@gmail.com>
2025-02-12 16:24:32 +0530

    Practice

---
+++ javacore/forloop/Program13.java
@@ -0,0 +1,12 @@
+package javacore.forloop;
+
+public class Program13 {
+    public static void main(String[] args) {
+        //wap to print all lower case alphabets.
+        System.out.println("Print all lower case alphabets.");
+
+        for(char i ='a';i<='z';i++){
+            System.out.println(i);
+        }
+    }
+}
