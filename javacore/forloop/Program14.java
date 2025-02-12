25f881fe97284aeae86cf844ca5fcc8c8f26dd94
Aameen-SE <aameen.se@gmail.com>
2025-02-12 16:24:42 +0530

    Practice

---
+++ javacore/forloop/Program14.java
@@ -0,0 +1,11 @@
+package javacore.forloop;
+
+public class Program14 {
+    public static void main(String[] args) {
+        //wap to print all upper case alphabets.
+        System.out.println("Print all upper case alphabets.");
+        for(char i = 'A';i<='Z';i++){
+            System.out.println(i);
+        }
+    }
+}
