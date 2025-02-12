f7d75ff5fa0bd05f292ceaa4ec08ccf19bda91ba
Aameen-SE <aameen.se@gmail.com>
2025-02-12 16:22:38 +0530

    Practice

---
+++ javacore/forloop/Program6.java
@@ -0,0 +1,12 @@
+package javacore.forloop;
+
+public class Program6 {
+    public static void main(String[] args) {
+        //wap to print all odd numbers between 120 to 100.
+        for(int i =120;i>=100;i--){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
