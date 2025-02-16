7eef0f61ba01db5395e8a7179feea95c9a7457c5
Aameen-SE <aameen.se@gmail.com>
2025-02-16 15:00:27 +0530

    Practice

---
+++ javacore/whileloop/FiveEvenNum.java
@@ -0,0 +1,15 @@
+package javacore.whileloop;
+
+public class FiveEvenNum {
+    public static void main(String[] args) {
+        //wap to print first five odd numbers.
+
+        int num=1;
+        while(num<=10){
+            if(num%2==0){
+                System.out.print(num+" , ");
+            }
+            num++;
+        }
+    }
+}
