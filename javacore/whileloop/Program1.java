ce63857eb4bf2bd6116a6324544fa61a584f5431
Aameen-SE <aameen.se@gmail.com>
2025-02-16 15:01:51 +0530

    Practice

---
+++ javacore/whileloop/Program1.java
@@ -0,0 +1,13 @@
+package javacore.whileloop;
+
+public class Program1 {
+    public static void main(String[] args) {
+        //wap to print number from 1 to 10
+        int num=1;
+
+        while(num<=10){
+            System.out.print(num+" , ");
+            num++;
+        }
+    }
+}
