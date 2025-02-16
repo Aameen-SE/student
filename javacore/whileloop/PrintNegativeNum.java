8e0f32c4f73bc26d88440ca235d912c6a65e5a99
Aameen-SE <aameen.se@gmail.com>
2025-02-16 15:01:14 +0530

    Practice

---
+++ javacore/whileloop/PrintNegativeNum.java
@@ -0,0 +1,15 @@
+package javacore.whileloop;
+
+public class PrintNegativeNum {
+    public static void main(String[] args) {
+        //wap  to print all negative numbers between -1 and -10.
+
+        int num =-1;
+
+        while(num>=-10){
+            System.out.print(num+" , ");
+            num--;
+        }
+
+    }
+}
