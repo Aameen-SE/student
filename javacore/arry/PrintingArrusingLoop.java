624d270acfe86e69cacfe2c943acf16e5e4ac2da
Aameen-SE <aameen.se@gmail.com>
2025-02-17 15:08:00 +0530

    Practice

---
+++ javacore/arry/PrintingArrusingLoop.java
@@ -0,0 +1,20 @@
+package javacore.arry;
+
+public class PrintingArrusingLoop {
+    public static void main(String[] args) {
+        //wap to create and print array using loop
+
+        int[] arr = new int[5];
+        arr[0]=1;
+        arr[1]=12;
+        arr[2]=123;
+        arr[3]=1234;
+        arr[4]=12345;
+
+        for(int i =0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
