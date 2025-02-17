3e0779ab1cd4c0e99cc51f09b18c17f34995a60c
Aameen-SE <aameen.se@gmail.com>
2025-02-17 15:08:35 +0530

    Practice

---
+++ javacore/arry/LengthofArray.java
@@ -0,0 +1,12 @@
+package javacore.arry;
+
+public class LengthofArray {
+    public static void main(String[] args) {
+        //wap to find length of an array
+        int arr[] = new int[10];
+        int arr2[] = {1,2,3,14,56,78,};
+
+        System.out.println("arr length is : "+arr.length);
+        System.out.println("arr2 length is : "+arr2.length);
+    }
+}
