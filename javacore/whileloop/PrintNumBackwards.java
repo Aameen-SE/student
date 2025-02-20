d6e9c0cfa42a93f6d8bb2d17e0238b8b8403314f
Aameen-SE <aameen.se@gmail.com>
2025-02-20 16:31:05 +0530

    Practice

---
+++ javacore/whileloop/PrintNumBackwards.java
@@ -0,0 +1,13 @@
+package javacore.whileloop;
+
+public class PrintNumBackwards {
+    public static void main(String[] args) {
+        //wap to print number from 50 to 45
+        int num=50;
+
+        while(num>=45){
+            System.out.print(num+" ");
+            num--;
+        }
+    }
+}
