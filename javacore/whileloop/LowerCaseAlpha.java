b9274acddac9ca45b5e58529af88f93ec700ef13
Aameen-SE <aameen.se@gmail.com>
2025-02-20 16:25:49 +0530

    Practice

---
+++ javacore/whileloop/LowerCaseAlpha.java
@@ -0,0 +1,14 @@
+package javacore.whileloop;
+
+public class LowerCaseAlpha {
+    public static void main(String[] args) {
+        //wap to print all lower case alphabets.
+        char c='a';
+
+        while (c <= 'z') {
+            System.out.print(c+" ");
+            c++;
+        }
+
+    }
+}
