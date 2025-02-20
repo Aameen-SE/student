ce915094442a5d1e0ea39eb1ece22405acee7359
Aameen-SE <aameen.se@gmail.com>
2025-02-20 16:26:04 +0530

    Practice

---
+++ javacore/whileloop/Multiple.java
@@ -0,0 +1,14 @@
+package javacore.whileloop;
+
+public class Multiple {
+    public static void main(String[] args) {
+        //wap to print all numbers between 1 to 150 multiple of 11.
+        int multipleOf=11;
+        int num=1;
+        while(num<=10){
+            System.out.println(multipleOf+" * "+ num + " = "+(multipleOf*num));
+            num++;
+        }
+
+    }
+}
