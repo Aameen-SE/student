05fd8453c351dceb9c07b227f1da5c52bc39724b
Aameen-SE <aameen.se@gmail.com>
2025-02-20 16:31:27 +0530

    Practice

---
+++ javacore/whileloop/UpperCaseAlpha.java
@@ -0,0 +1,14 @@
+package javacore.whileloop;
+
+public class UpperCaseAlpha {
+    public static void main(String[] args) {
+        //wap to print all upper case alphabets.
+
+        char c='A';
+
+        while(c<='Z'){
+            System.out.print(c+" ");
+            c++;
+        }
+    }
+}
