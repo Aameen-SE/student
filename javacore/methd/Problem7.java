9782d363e3c3144af70d9e25f8376bfc38b2d5c7
Aameen-SE <aameen.se@gmail.com>
2025-02-22 16:45:08 +0530

    Practice

---
+++ javacore/methd/Problem7.java
@@ -0,0 +1,14 @@
+package javacore.methd;
+
+public class Problem7 {
+    public static void main(String[] args) {
+        //int add(int, int) - add two numbers and return sum
+
+        int num1=12,num2=98;
+        System.out.println("Sum of the two number : "+sum(num1,num2));
+
+    }
+    public static int sum(int a,int b){
+        return a+b;
+    }
+}
