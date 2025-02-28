d26e770272c057d3c208d9066f5973aca694eb66
Aameen-SE <aameen.se@gmail.com>
2025-02-28 16:04:56 +0530

    parameterized constructor and default constructor and practising using inheritance

--- javacore/InheritanRevise.java
+++ javacore/InheritanRevise.java
@@ -3,14 +3,21 @@ class A{
     public A(){
         System.out.println("in A");
     }
+    public A(int n){
+        System.out.println("in A int ");
+    }
 }
 
 class B extends A{
     public B(){
+        super(65);
+        //super();
         System.out.println("in B ");
     }
 
     public B(int n){
+        this();
+      //  super(n);
         System.out.println("in B int ");
     }
 }
