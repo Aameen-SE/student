bb8048d8fba6b3a1924b51d1a80fd9ba23e88b9a
Aameen-SE <aameen.se@gmail.com>
2025-05-03 16:42:07 +0530

    practice of today on 3 may

---
+++ jva/may03/PresentationMain.java
@@ -0,0 +1,8 @@
+package jva.may03;
+
+public class PresentationMain {
+    public static void main(String[] args) {
+        Presentation presentation= new Presentation(123,456,879);
+        System.out.println(presentation.toString());
+    }
+}
