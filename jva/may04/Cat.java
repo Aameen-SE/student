63c4cea4f56a5a2fefc58e5678f60c8f1ea34422
Aameen-SE <aameen.se@gmail.com>
2025-05-04 19:24:06 +0530

    practice of today on 4 may

---
+++ jva/may04/Cat.java
@@ -0,0 +1,8 @@
+package jva.may04;
+//hierarical inheritance
+public class Cat  extends Animal{
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.eat();
+    }
+}
