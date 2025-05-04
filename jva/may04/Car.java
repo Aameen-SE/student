f531e01f662a4d6a4b588a74eaa59f2297d2947b
Aameen-SE <aameen.se@gmail.com>
2025-05-04 19:23:54 +0530

    practice of today on 4 may

---
+++ jva/may04/Car.java
@@ -0,0 +1,10 @@
+package jva.may04;
+class Engine{
+
+}
+public class Car {
+    public static void main(String[] args) {
+        // association inheritance HAS-A
+        Engine e= new Engine();
+    }
+}
