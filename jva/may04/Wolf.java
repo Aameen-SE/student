071b91a62c0df2db3de508c6066fddae393adbb6
Aameen-SE <aameen.se@gmail.com>
2025-05-04 19:24:39 +0530

    practice of today on 4 may

---
+++ jva/may04/Wolf.java
@@ -0,0 +1,10 @@
+package jva.may04;
+// single Inheritance
+// heriarchal inheritance
+public class Wolf extends Animal{
+    public static void main(String[] args) {
+        Wolf wolf=new Wolf();
+        wolf.howl();
+        wolf.eat();
+    }
+}
