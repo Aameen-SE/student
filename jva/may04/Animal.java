bfb4adb209c1efd3ccb877af274a0e4bced924bb
Aameen-SE <aameen.se@gmail.com>
2025-05-04 19:23:29 +0530

    practice of today on 4 may

---
+++ jva/may04/Animal.java
@@ -0,0 +1,15 @@
+package jva.may04;
+
+public class Animal {
+    // parent class or super class
+    void eat(){
+        System.out.println("Eating .... ");
+    }
+    void bark(){
+        System.out.println("Barking");
+    }
+    void howl(){
+        System.out.println("howling....");
+    }
+}
+
