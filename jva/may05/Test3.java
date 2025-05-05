d9c54bada6b6e4e0becbb744c29e3e013ee239ec
Aameen-SE <aameen.se@gmail.com>
2025-05-05 18:01:30 +0530

    practice of today on 5 may

---
+++ jva/may05/Test3.java
@@ -0,0 +1,10 @@
+package jva.may05;
+
+public class Test3 {
+    void show(){
+        System.out.println("1");
+    }
+    void print(String a ,int b){
+        System.out.println("int , String");
+    }
+}
