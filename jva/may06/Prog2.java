8a0b7acdae3344777a81bfd9ebee669cc1054202
Aameen-SE <aameen.se@gmail.com>
2025-05-06 19:45:53 +0530

    practice of today on 6 may

---
+++ jva/may06/Prog2.java
@@ -0,0 +1,10 @@
+package jva.may06;
+abstract  class Abstrac{
+    void show(){}
+}
+public class Prog2 extends Abstrac{
+    public static void main(String[] args) {
+        Prog2 prog = new Prog2();
+        prog.show();
+    }
+}
