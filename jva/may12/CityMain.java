2bd0b905e30c2c19ac5018372af193e1c575c1c4
Aameen-SE <aameen.se@gmail.com>
2025-05-12 13:14:10 +0530

    practice of today on 12 may

---
+++ jva/may12/CityMain.java
@@ -0,0 +1,11 @@
+package jva.may12;
+
+public class CityMain {
+
+    public static void main(String[] args) {
+
+        City city = new City(123,"Kolkata");
+
+        System.out.println("Id : "+city.getId()+" , City : "+city.getCity());
+    }
+}
