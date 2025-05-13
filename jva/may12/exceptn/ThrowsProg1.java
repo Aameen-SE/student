506ef43772e35886b2d4c687e916e457049ac71a
Aameen-SE <aameen.se@gmail.com>
2025-05-13 16:26:10 +0530

    practice of today on 13 may

---
+++ jva/may12/exceptn/ThrowsProg1.java
@@ -0,0 +1,13 @@
+package jva.may12.exceptn;
+
+public class ThrowsProg1 {
+    public static void main(String[] args) throws Exception{
+        // throws keyword is used to just informed that it may throw exception
+        int a=0;
+        int b=90;
+        int c= b/a;
+        System.out.println(c);
+
+        System.out.println("Hey there ");
+    }
+}
