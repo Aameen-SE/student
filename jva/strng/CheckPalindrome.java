7b6d51c5c5af648651f1c7d35fa27df6e59eadee
Aameen-SE <aameen.se@gmail.com>
2025-06-11 14:30:35 +0530

    practice on 11 june

---
+++ jva/strng/CheckPalindrome.java
@@ -0,0 +1,12 @@
+package jva.strng;
+
+import java.util.Scanner;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string ");
+        String word = sc.nextLine();
+
+    }
+}
