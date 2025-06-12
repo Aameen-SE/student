a6bbeb66a40ecfc3017c93d3aa26909dfb7410de
Aameen-SE <aameen.se@gmail.com>
2025-06-12 19:12:07 +0530

    practice on 12 june

--- jva/strng/CheckPalindrome.java
+++ jva/strng/CheckPalindrome.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 
 public class CheckPalindrome {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the string ");
         String word = sc.nextLine();
+        word=word.toLowerCase();
 
+        String  reverse ="";
+
+        for(int i=word.length()-1;i>=0;i--){
+            reverse=reverse+word.charAt(i);
+        }
+
+        if(word.equals(reverse))
+            System.out.println("It is a palindrome");
+        else
+            System.out.println("Not a palindrome ");
     }
 }
