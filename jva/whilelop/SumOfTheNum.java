43a7d8332af22743fe20c8de41cf7983e5b8540b
Aameen-SE <aameen.se@gmail.com>
2025-06-16 17:40:27 +0530

    practice on 16 june

---
+++ jva/whilelop/SumOfTheNum.java
@@ -0,0 +1,20 @@
+package jva.whilelop;
+
+import java.util.Scanner;
+
+public class SumOfTheNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num= sc.nextInt();
+
+        int sum=0;
+
+        while(num!=0){
+            sum = sum+num%10;
+            num=num/10;
+        }
+
+        System.out.println("Sum of the given number is : "+sum);
+    }
+}
