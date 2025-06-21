9a6047fb2427c26aeca61ce3a62d3d6d7c7e9743
Aameen-SE <aameen.se@gmail.com>
2025-06-21 15:48:18 +0530

    practice on 21 june

---
+++ jva/forlop/DivBy3.java
@@ -0,0 +1,11 @@
+package jva.forlop;
+
+public class DivBy3 {
+    public static void main(String[] args) {
+        System.out.println("Num divi by 3 from 1 to 10 : ");
+        for(int i=1;i<=10;i++){
+            if(i%3==0)
+                System.out.print(i+" ");
+        }
+    }
+}
