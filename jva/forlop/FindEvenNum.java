9b0c39709a4802833ddf016b000beec09f872634
Aameen-SE <aameen.se@gmail.com>
2025-06-21 15:48:29 +0530

    practice on 21 june

---
+++ jva/forlop/FindEvenNum.java
@@ -0,0 +1,11 @@
+package jva.forlop;
+
+public class FindEvenNum {
+    public static void main(String[] args) {
+        System.out.println(" All Even numbers between 100 to 120.");
+        for(int i=100;i<=120;i++){
+            if(i%2==0)
+                System.out.print(i+" ");
+        }
+    }
+}
