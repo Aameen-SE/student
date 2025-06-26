7f75c22e7d71ee71fd2361ffa6be685ac259159f
Aameen-SE <aameen.se@gmail.com>
2025-06-26 12:49:49 +0530

    practice on 26 june on typecasting

--- numberingsystem/binarytodecimal/Prog2.java
+++ numberingsystem/binarytodecimal/Prog2.java
@@ -2,9 +2,8 @@ package numberingsystem.binarytodecimal;
 
 public class Prog2 {
     public static void main(String[] args) {
-        String input = "1111";
-        System.out.println("Binary number : "+input);
-        int decimalValue = Integer.parseInt(input,2);
-        System.out.println("Decimal : "+decimalValue);
+       String input ="1010";
+       int decimal = Integer.parseInt(input,2);
+        System.out.println("Decimal Value : "+decimal);
     }
 }
