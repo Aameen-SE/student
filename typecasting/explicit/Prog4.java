71d3237cdbaa8b23a2e5ee219f24c7e91da89599
Aameen-SE <aameen.se@gmail.com>
2025-06-26 12:50:23 +0530

    practice on 26 june on typecasting

---
+++ typecasting/explicit/Prog4.java
@@ -0,0 +1,10 @@
+package typecasting.explicit;
+
+public class Prog4 {
+    //Cast long to int and print.
+    public static void main(String[] args) {
+        long var = 45l;
+        int varInt = (int)var;
+        System.out.println(varInt);
+    }
+}
---
+++ typecasting/implicit/Prog4.java
@@ -0,0 +1,10 @@
+package typecasting.implicit;
+
+public class Prog4 {
+    //Divide two int values and store result in float
+    public static void main(String[] args) {
+        int a = 125, b=5;
+        float result = a/b;
+        System.out.println("Result : "+result);
+    }
+}
