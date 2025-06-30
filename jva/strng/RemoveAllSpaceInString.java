afa4a67ff2132605d1dd2af5fc349b82dd1f6f9c
Aameen-SE <aameen.se@gmail.com>
2025-06-30 14:14:53 +0530

    Wap to remove all spaces from a string

---
+++ jva/strng/RemoveAllSpaceInString.java
@@ -0,0 +1,11 @@
+package jva.strng;
+
+public class RemoveAllSpaceInString {
+    public static void main(String[] args) {
+        //Wap to remove all spaces from a string
+
+        String input = "I am a Software Engineer";
+        String result =input.replace(" ","");
+        System.out.println(result);
+    }
+}
