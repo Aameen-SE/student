d94c5a1e5c85b2a2efe4ef78fec8bce7749741eb
Aameen-SE <aameen.se@gmail.com>
2025-07-02 14:04:31 +0530

    on july02

--- training/HelloWorld.java
+++ training/HelloWorld.java
@@ -4,8 +4,18 @@ public class HelloWorld {
     public static void main(String[] args) {
         // primtive data type
         // int short long char byte  float double boolean
-        //a , b  same tyepe integer
+                    //a , b  same tyepe integer
 
-       
+        int q=111,R=333;
+        int q1 =111, r=333;
+        
+       //PascalCase
+        // class and interface
+        // HelloWorld
+
+
+        //CamelCase - camelCase
+        //variableMethod
+        // helloWorld
     }
 }
