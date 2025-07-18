8478f00b428127c4dcb457d8cafd9d0cf27eb7d6
Aameen-SE <aameen.se@gmail.com>
2025-07-18 20:01:57 +0530

    String program

---
+++ dailypractice/july18/RemoveSpaceFromStrng.java
@@ -0,0 +1,11 @@
+package dailypractice.july18;
+
+public class RemoveSpaceFromStrng {
+
+    public static void main(String[] args) {
+
+        String str = "H  e  y t  he  re Peo  ple";
+       str= str.replace(" ","");
+        System.out.println(str);
+    }
+}
