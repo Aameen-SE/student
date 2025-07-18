799b686a59ea53f2fee55e80966475348e5f8ec9
Aameen-SE <aameen.se@gmail.com>
2025-07-18 19:59:21 +0530

    String program

---
+++ dailypractice/july18/CharToStrng.java
@@ -0,0 +1,11 @@
+package dailypractice.july18;
+
+public class CharToStrng {
+
+    public static void main(String[] args) {
+
+        char[] ch = {'h','e','l','l','o'};
+        String str = new String(ch);
+        System.out.println(ch);
+    }
+}
