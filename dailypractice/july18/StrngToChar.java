8b5b3d6dc23c6a04460498c280dbfa4406ee8e0a
Aameen-SE <aameen.se@gmail.com>
2025-07-18 20:02:16 +0530

    String program

---
+++ dailypractice/july18/StrngToChar.java
@@ -0,0 +1,14 @@
+package dailypractice.july18;
+
+public class StrngToChar {
+
+    public static void main(String[] args) {
+
+        String str = "Input";
+        char[] ch = str.toCharArray();
+
+        for (char c :ch){
+            System.out.print(c+" ");
+        }
+    }
+}
