bec26104657d346f85f3d657253adbdaa22eb596
Aameen-SE <aameen.se@gmail.com>
2025-07-24 18:36:00 +0530

    practice program

---
+++ dailypractice/july19/FactorialOfEachElement.java
@@ -0,0 +1,17 @@
+package dailypractice.july19;
+
+import java.util.Arrays;
+
+public class FactorialOfEachElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println("Array: "+ Arrays.toString(arr));
+        int fact=1;
+
+     
+
+    }
+
+}
