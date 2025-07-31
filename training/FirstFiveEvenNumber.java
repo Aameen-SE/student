0c5d2fe5d70bb038a2fbd43ee0e90809e9919580
Aameen-SE <aameen.se@gmail.com>
2025-07-31 13:24:24 +0530

    july31 practice

---
+++ training/FirstFiveEvenNumber.java
@@ -0,0 +1,13 @@
+package training;
+
+public class FirstFiveEvenNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Even Number c");
+        for(int i=1;i<=15;i++){
+            if(i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
