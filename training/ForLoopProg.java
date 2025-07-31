d4fff00488ee9228f52da2907258813a7877f453
Aameen-SE <aameen.se@gmail.com>
2025-07-31 13:24:34 +0530

    july31 practice

---
+++ training/ForLoopProg.java
@@ -0,0 +1,18 @@
+package training;
+
+public class ForLoopProg {
+    public static void main(String[] args) {
+       //forloop
+        // for(int i=1;i<=10;i++){
+        // sout("Hello World")
+        // }
+        int n =13;
+        //System.out.println("Hello World");
+// for(initi, condition,increement/decreement)
+
+        for(int i=10;i>=1;i--){
+            System.out.println("Hello World "+i);
+        }
+
+    }
+}
