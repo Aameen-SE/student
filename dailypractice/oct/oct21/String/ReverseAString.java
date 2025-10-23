ebb1ddc5d8d2094b0ce1f4f6cfc7bbec15f4b687
Aameen-SE <aameen.se@gmail.com>
2025-10-23 15:24:59 +0530

    Reverse A string

---
+++ dailypractice/oct/oct21/String/ReverseAString.java
@@ -0,0 +1,18 @@
+package dailypractice.oct.oct21.String;
+
+public class ReverseAString {
+
+    public static void main(String[] args) {
+
+        String str = "Aameen";
+
+        String reverse = "";
+
+        for(int i =str.length()-1;i>=0;i--)
+        {
+            reverse=reverse+ str.charAt(i);
+        }
+
+        System.out.println("Reverse String : "+reverse);
+    }
+}
