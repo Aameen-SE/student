1d1eb188afd9cbe43de446ed00bc7187de7a55e0
Aameen-SE <aameen.se@gmail.com>
2025-11-22 19:04:07 +0530

    /8️⃣ Default Method in Interface // //Create an interface Vehicle with: // //abstract method start() // //default method fuelType() // //Implement this interface in class Car.

---
+++ dailypractice/nov/nov22/CalculatorMain.java
@@ -0,0 +1,9 @@
+package dailypractice.nov.nov22;
+//Create your own functional interface
+// Calculator with a method add(a,b) and implement it using a lambda expression.
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+
+    }
+}
