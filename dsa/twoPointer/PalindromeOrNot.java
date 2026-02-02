ff9ef47f851ebfec2e8f73ff0f26ba7ca12459fd
Aameen <aameen.se@gmail.com>
2026-02-02 15:07:32 +0530

    PalindromeOrNot using two pointer

---
+++ dsa/twoPointer/PalindromeOrNot.java
@@ -0,0 +1,23 @@
+package dsa.twoPointer;
+
+public class PalindromeOrNot {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1};
+
+        int left = 0;
+        int right = arr.length-1;
+        boolean isPalindrome = true;
+        while (left<right){
+            if(arr[left]!=arr[right]){
+                isPalindrome=false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.println("Palindrome Array : "+isPalindrome);
+
+    }
+}
