91f6e6d315354cfecf9db342c556b68f4665f35c
Aameen <aameen.se@gmail.com>
2026-02-02 15:07:15 +0530

    Move All Zero using two pointer

---
+++ dsa/twoPointer/MoveAllZero.java
@@ -0,0 +1,22 @@
+package dsa.twoPointer;
+
+import java.util.Arrays;
+
+public class MoveAllZero {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+
+        int j =0;
+
+        for (int i =0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+
+        System.out.println("Array : "+ Arrays.toString(nums));
+    }
+}
