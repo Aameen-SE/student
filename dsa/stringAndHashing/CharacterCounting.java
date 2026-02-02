18960af4f0d0d1c95b008fdfaaa67e4c166ad6e1
Aameen <aameen.se@gmail.com>
2026-02-02 15:06:56 +0530

    Character Counting

---
+++ dsa/stringAndHashing/CharacterCounting.java
@@ -0,0 +1,18 @@
+package dsa.stringAndHashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterCounting {
+    public static void main(String[] args) {
+        String s = "banana";
+
+        Map<Character,Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        System.out.println(map);
+    }
+}
