a9bc81094681460f679fcef721be5d677435500d
Aameen <aameen.se@gmail.com>
2026-02-11 20:23:34 +0530

    Inheritance Related Program

---
+++ practice2026/oop/inheritanc/Child.java
@@ -0,0 +1,13 @@
+package practice2026.oop.inheritanc;
+
+public class Child extends Parent{
+
+    public Child(){
+        System.out.println("Child constructor is caLLED");
+    }
+
+    public static void main(String[] args) {
+        Child child = new Child();
+
+    }
+}
---
+++ practice2026/oop/inheritanc/Dog.java
@@ -0,0 +1,9 @@
+package practice2026.oop.inheritanc;
+
+public class Dog extends Animal{
+
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating ");
+    }
+}
