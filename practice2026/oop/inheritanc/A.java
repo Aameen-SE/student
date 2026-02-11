08e391f6e870fafa04d256335e5358e3a83003b5
Aameen <aameen.se@gmail.com>
2026-02-11 20:22:33 +0530

    Inheritance Related Program

---
+++ practice2026/oop/inheritanc/A.java
@@ -0,0 +1,8 @@
+package practice2026.oop.inheritanc;
+
+public class A {
+
+    public void methodA(){
+        System.out.println("A method is called");
+    }
+}
---
+++ practice2026/oop/inheritanc/Animal.java
@@ -0,0 +1,9 @@
+package practice2026.oop.inheritanc;
+
+public class Animal {
+
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+
+}
