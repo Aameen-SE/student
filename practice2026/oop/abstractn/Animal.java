1feaa87b06a40b93c1ce27ff8d7042e6511cfb48
Aameen <aameen.se@gmail.com>
2026-02-12 13:26:55 +0530

    Abstraction Related Program

---
+++ practice2026/oop/abstractn/Animal.java
@@ -0,0 +1,10 @@
+package practice2026.oop.abstractn;
+
+public abstract class Animal {
+    abstract void sound();
+
+    public void sleeping(){
+        System.out.println("Animal is sleeping");
+    }
+
+}
