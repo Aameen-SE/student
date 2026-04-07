0d13fa242759fba96fe4583d3efc4d818cbf811c
Aameen <aameen.se@gmail.com>
2026-04-07 15:13:58 +0530

    DSA Problem : TwoSum

---
+++ days90/day01/TwoSum.java
@@ -0,0 +1,31 @@
+package days90.day01;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int [] nums = {3,2,4};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i< nums.length;i++){
+            int c = target -nums[i];
+
+            if(map.containsKey(c)){
+                return new int[] {map.get(c),i};
+            }
+            map.put(nums[i],i);
+        }
+return null;
+
+    }
+}
