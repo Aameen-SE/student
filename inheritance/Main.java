656286bf5b42c61c03319768be878cbbed77cfe7
Aameen <aameen.se@gmail.com>
2026-04-07 15:12:43 +0530

    Program

---
+++ inheritance/Main.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.bark();
+        dog.eat();
+
+
+        Animal dog1 = new Dog();
+        dog1.eat();
+
+    }
+}
